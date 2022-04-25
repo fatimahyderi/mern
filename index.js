4cac6cc7d0a44e62a751e6e7757689eedfc9786a
fatimahyderi <fatimahyderi84@gmail.com>
2022-04-25 00:57:02 -0700

    class 9 nodemon

--- index.js
+++
@@ -1,7 +0,0 @@
-let num_array =[34,45,67,54,34];
-let total = 0;
-for(i = 0; i < num_array.length; i++){
-    num = num_array[i];
-    total = total + num;
-}
- console.log(total);
---
+++ mern 9/index.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import http from 'http';
+
+http.createServer(function(req, res) {
+  var writeStream = fs.createWriteStream('./output');
+
+  req.pipe(writeStream);
+
+  req.on('end', function () {
+    res.writeHead(200, {"content-type":"text/html"});
+    res.end('<form method="POST"><input name="test" /><input type="submit"></form>');
+  });
+
+  writeStream.on('error', function (err) {
+    console.log(err);
+  });
+}).listen(8080);
