0f00ab5a0744860f44ff9d1079b1036664faefae
fatimahyderi <fatimakhurram84@gmail.com>
2022-04-28 03:34:17 +0500

    assignment name

--- mern 10/index.js
+++ mern 10/index.js
@@ -1,22 +1,21 @@
 import http from 'http';
 import fs from 'fs';
+import url from 'url';
 
 http.createServer(function(req,res){
-    let filename = req.url;
-    let folderarray = fs.readdir('.',(files));
-    for(let i =0; i<folderarray.length;i++){
-        if (folderarray[i] === filename){
-            fs.readFile('./about.html','utf8',(err,data) =>{
-                         if(err) throw err;
-                         res.end(data);
-                    })
-        }
-    }
     
-    // if(req.url === '/about'){
-    //     fs.readFile('./about.html','utf8',(err,data) =>{
-    //         if(err) throw err;
-    //         res.end(data);
-    //     })
-    // }
+const address = 'https://geeksforgeeks.org/projects?sort=newest&lang=nodejs';
+  
+let urlObject = url.parse(address, true);
+  
+console.log('URL Object returned after parsing');
+  
+console.log(urlObject)
+
+    //  if(req.url === '/about'){
+    //      fs.readFile('./about.html','utf8',(err,data) =>{
+    //          if(err) throw err;
+    //          res.end(data);
+    //      })
+    //  }
 }).listen(8080);
