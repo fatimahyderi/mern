6e8eef371fcb7688d07a88a1b705a795a1d037b7
fatimahyderi <fatimahyderi84@gmail.com>
2022-05-12 01:42:33 -0700

    eslint

---
+++ spread operators/index.js
@@ -0,0 +1,19 @@
+const num = [1,2,3,4,5,6,7,8,9,10]
+
+// for(let i = 0; i < num.length ;i++){
+//     console.log(num[i]*2)
+// }
+
+const [a,...soon] = num;
+console.log(soon[3]                                                                                                                           *5);
+
+
+// function multiples (a,...numbers){
+//     for(let i = 0; i < numbers.length ;i++){
+//              console.log(numbers[i]*a)
+//          }
+//  };
+
+ //multiples(2,num);
+
+var testing = "hello";
