6294e2455b4de6245425bdb1f5de4b3708cee43c
fatimahyderi <fatimahyderi84@gmail.com>
2022-05-17 01:09:20 -0700

    promise and hell

---
+++ promise class 18/index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+console.log("server 8080");
+app.get('/',function(req,res){
+    
+    const getsalary = () => {
+        let salaries = [3000,4000,5000]
+        return new Promise((resolve,reject) => {
+            resolve(salaries[1])
+        });
+    }
+
+    const getdata = () => {
+        let data = {
+            '1':{
+                fname:'ali',
+                lname:'ahmed',
+                age:25
+            }
+        }
+            return new Promise((resolve,reject) => {
+                resolve(data[1])
+            });
+        
+    }
+
+    const getmsg = () => {
+        let msg ='Salary transfer';
+        return new Promise((resolve,reject) => {
+            resolve(msg)
+        });
+    }
+
+    let salary1
+    let user1
+    getsalary().then((salary) => {
+        console.log(salary);
+        salary1 = salary;
+        return getdata();
+    }).then((user) => {
+        console.log(user);
+        user1 = user.fname
+        console.log(user1)
+        return getmsg();
+    }).then((msg) => {
+        console.log(`${salary1} ${msg} to ${user1} `);
+    })
+})
+
+app.listen(port);
