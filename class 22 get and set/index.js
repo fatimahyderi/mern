f34eaf7c58fcf3f500328a8768ae3e21b9049a6d
fatimahyderi <fatimakhurram84@gmail.com>
2022-05-26 19:08:57 +0500

    set n get

--- class 22 get and set/index.js
+++ class 22 get and set/index.js
@@ -11,10 +11,6 @@ class User{
 
 
 class Admin extends User{
-    constructor(role){
-        super();
-        this.role = role;
-    }
 
     get password_value(){
         return `${this.password}`
@@ -22,9 +18,10 @@ class Admin extends User{
 
     set password_value(arg){
         if(arg.length > 6){
-            throw 'Max 6 characters allowed';
+            console.log('Max 6 characters allowed');
         }else {
             this.password = arg;
+            console.log('password changed')
         }
     }
 
@@ -33,10 +30,16 @@ class Admin extends User{
 
 }
 
-let user1 = new User("ali",23,"password");
-console.log(user1);
+
+let admin1 = new Admin("ali",23,"admin12345");
+console.log(admin1.password_value);
+admin1.password_value = 'pass';
+console.log(admin1.password_value)
+
+//let user1 = new User("ali",23,"password");
+//console.log(user1);
 // let admin1 = new Admin("John",30,"pass","admin");
 // console.log(user1.password_value)
-let admin1 = new Admin()
-console.log(admin1)
+//let admin1 = new Admin()
+//console.log(admin1)
 //admin1.password_value= 'passwordchanged';
