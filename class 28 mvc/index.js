221a0d956c9f919123b461fc9148c09269b41f05
fatimahyderi <fatimakhurram84@gmail.com>
2022-06-04 05:49:59 +0500

    mvc

--- class 28 mvc/index.js
+++ class 28 mvc/index.js
@@ -1,11 +1,18 @@
 
 import express from 'express';
+import bodyParser from 'body-parser';
 import 'dotenv/config';
 import {router} from './routes/route.js'
 const app = express();
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(router);
+app.use(express.static('public'));
+app.use('/static',express.static('public'));
+app.use('/static',express.static('public/stylesheet'));
 
 app.set('view engine','ejs');
 
+console.log(process.env.port);
 app.listen(process.env.port);
--- class 28 mvc/routes/route.js
+++ class 28 mvc/routes/route.js
@@ -1,9 +1,37 @@
 import express from "express";
+import bodyParser from 'body-parser';
 const router = express.Router();
+const products = [];
 
 
 router.get('/add',function(req,res){
     res.render('add');
 })
+
+router.post('/add',function(req,res){
+   // console.log(req.body)
+    products.push({
+        productname : req.body.productname,
+        brand: req.body.brand,
+        model: req.body.model
+    });
+    res.redirect('/')
+    //console.log(products);
+})
+
+router.get('/edit',function(req,res){
+    res.render('edit');
+})
+
+router.post('/edit',function(req,res){
+    //console.log(req.body);
+})
+
+router.get('/',function(req,res){
+    //console.log(products)
+    res.render('view',{
+        products : products
+    });
+});
 
 export {router}
